87b6c5b131600a849572240de5ccb870116e4cb7
l1on3hart88 <spenccheng@gmail.com>
2015-07-06 22:16:53 +0000

    99% done with home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
-$(document).ready(function(){
+$(document).on('page:load',(function(){
  $(".dropdown").hover(            
   function() {
       $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
       $(this).toggleClass('open');
-      $('b', this).toggleClass("caret caret-up");                
+//       $('b', this).toggleClass("caret caret-up");                
   },
   function() {
       $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
       $(this).toggleClass('open');
-      $('b', this).toggleClass("caret caret-up");                
+//       $('b', this).toggleClass("caret caret-up");                
  });
   
 
