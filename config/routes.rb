448e035218ac4a0886001ab54e957c3ba9469a0d
l1on3hart88 <spenccheng@gmail.com>
2015-08-05 17:20:31 +0000

    iPad unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+
+  # Make sure this routeset is defined last
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -61,4 +65,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
